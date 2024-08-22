6535ab093ca513c8e7e3b23a3c5f9acd1bdb1bfc
Jonathan Alexis Islas Estrada <L24550378@chihuahua2.tecnm.mx>
2024-08-22 16:32:19 -0600

    Add files via upload

---
+++ EVA1_1_HOLA_MUNDO/src/main/java/com/mycompany/eva1_1_hola_mundo/EVA1_1_HOLA_MUNDO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_1_hola_mundo;
+
+/**
+ *
+ * @author ae478
+ */
+public class EVA1_1_HOLA_MUNDO {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo!!");
+    }
+}
