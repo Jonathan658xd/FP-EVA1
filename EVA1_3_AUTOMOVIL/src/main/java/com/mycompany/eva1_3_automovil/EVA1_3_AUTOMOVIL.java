f0195622b937e9bcdaef49ac8d1216e544843e74
Jonathan Alexis Islas Estrada <L24550378@chihuahua2.tecnm.mx>
2024-08-27 23:05:03 -0600

    Add files via upload

---
+++ EVA1_3_AUTOMOVIL/src/main/java/com/mycompany/eva1_3_automovil/EVA1_3_AUTOMOVIL.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_3_automovil;
+
+/**
+ *
+ * @author ae478
+ */
+public class EVA1_3_AUTOMOVIL {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        //DECLARACION DE LAS VARIABLES
+        String marca;
+        String modelo;
+        int year;
+        double precio;
+        
+        //ASIGNAR VALORES
+        marca = "Ford";
+        modelo = "Mustang";
+        year = 2022;
+        precio = 999000.1;
+        
+        System.out.println(marca);
+        System.out.println(modelo);
+        System.out.println(year);
+        System.out.println(precio);
+    }
+    
+}
