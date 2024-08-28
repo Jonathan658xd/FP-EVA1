feb29d7fac2328ff0029d589906ccddd402f71d4
Jonathan Alexis Islas Estrada <L24550378@chihuahua2.tecnm.mx>
2024-08-27 23:00:46 -0600

    Add files via upload

---
+++ EVA1_4_TELEVISION/src/main/java/com/mycompany/eva1_4_television/EVA1_4_TELEVISION.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_4_television;
+
+/**
+ *
+ * @author ae478
+ */
+public class EVA1_4_TELEVISION {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        String marca;
+        String modelo;
+        String tecno;
+        int tamaño;
+        double precio;
+        
+        marca = "Samsung";
+        modelo = "Oled 4K";
+        tecno = "Led";
+        tamaño = 70;
+        precio = 20000;
+        
+        System.out.println(marca);
+        System.out.println(modelo);
+        System.out.println(tecno);
+        System.out.println(tamaño);
+        System.out.println(precio);
+     }
+      
+ }
