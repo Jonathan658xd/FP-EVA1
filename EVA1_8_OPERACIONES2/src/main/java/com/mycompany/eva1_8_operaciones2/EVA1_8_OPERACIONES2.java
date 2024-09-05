84d26b80030438b482eef6826f27d1b20fdd95e9
Jonathan Alexis Islas Estrada <L24550378@chihuahua2.tecnm.mx>
2024-09-04 23:02:33 -0600

    Add files via upload

---
+++ EVA1_8_OPERACIONES2/src/main/java/com/mycompany/eva1_8_operaciones2/EVA1_8_OPERACIONES2.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_8_operaciones2;
+
+/**
+ *
+ * @author ae478
+ */
+public class EVA1_8_OPERACIONES2 {
+
+    public static void main(String[] args) {
+        String nombre = "Jonathan";
+        String apellido = "Estrada";
+        String nombreCom;
+        //CADENAS DE TEXTO, LA OPERACION SE LLAMA
+        //CONCATENACION -- UNIR CADENAS DE TEXTO
+        //El operador de concatenacion es el +
+        //cuando un simbolo tiene varias funciones
+        //se le llama sobrecarga (de operadores)
+        nombreCom = nombre + " " + apellido;
+        
+        System.out.println("El nombre es: " + nombre);
+        System.out.println("El apellido es: " + apellido);
+        System.out.println("El nombre completo es: " + nombreCom);
+        
+    
+        
+    }
+}
