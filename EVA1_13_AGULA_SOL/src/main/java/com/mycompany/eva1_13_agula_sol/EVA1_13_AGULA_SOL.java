f4395fb5068620286b29d5f39e94f3e5cd64d09d
Jonathan Alexis Islas Estrada <L24550378@chihuahua2.tecnm.mx>
2024-09-11 00:27:12 -0600

    Add files via upload

---
+++ EVA1_13_AGULA_SOL/src/main/java/com/mycompany/eva1_13_agula_sol/EVA1_13_AGULA_SOL.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_13_agula_sol;
+
+/**
+ *
+ * @author ae478
+ */
+public class EVA1_13_AGULA_SOL {
+
+    public static void main(String[] args) {
+         double valor = Math.random();
+         //random genera valor >= 0 < a 1
+         //entre y 0 .9999999999999
+         //0123456789 
+         if(valor > 0.5){
+         System.out.println("Aguila");
+       }else{
+               
+         System.out.println("Sol");}
+         
+    }
+}
